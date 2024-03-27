acb3df59ea7fb7ad68981bab682dff0124de8213
rishabhgawade <130897445+rishabhgawade@users.noreply.github.com>
2024-03-27 16:21:23 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
              <li><a href="about.php"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>About</a></li>
              <li><a href="services.php"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>Services</a></li>
              <li><a href="contact.php"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Contact</a></li>
-             <li><a href="admin/login.php"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>Admin</a></li>
+             <li><a href="login.php"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>Admin</a></li>
              </ul>
          </div>
          <div class="clearfix"></div>
